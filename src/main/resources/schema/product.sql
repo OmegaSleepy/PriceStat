0ecb9c47d093d88973ffbac5c7d6171512a09b2f
martin <martingeorgiev2307@gmail.com>
2026-05-10 20:05:26 +0300

    fix: add product.sql

---
+++ src/main/resources/schema/product.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS product (
+    city TEXT,
+    shop_address TEXT,
+    product_name TEXT,
+    product_id TEXT,
+    product_category TEXT,
+    product_price double precision,
+    promo_price double precision,
+    firm TEXT
+);
