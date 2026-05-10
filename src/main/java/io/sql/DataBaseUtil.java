93d6320cb4a59a90f0ba8fc16811f6aa15dc8d7b
martin <martingeorgiev2307@gmail.com>
2026-05-10 20:44:33 +0300

    feat: central connection util (don't have to retype credentials)

---
+++ src/main/java/io/sql/DataBaseUtil.java
@@ -0,0 +1,16 @@
+package io.sql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseUtil {
+
+    private static final String url = "jdbc:postgresql://localhost:5432/appdb";
+    private static final String user = "appuser";
+    private static final String password = "apppassword";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+}
