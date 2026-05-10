d0795578d2d14e2563e0c706d26d6c738bfa6c96
martin <martingeorgiev2307@gmail.com>
2026-05-10 15:22:45 +0300

    feat: ZipReader for reading Zips

---
+++ src/main/java/io/zip/ZipReader.java
@@ -0,0 +1,59 @@
+package io.zip;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class ZipReader {
+
+    private final File file;
+
+    public ZipReader (File file) {
+        this.file = file;
+    }
+
+    public Map<String, String> getZipAsNameContentsMap() {
+
+        Map<String, String> result = new HashMap<>();
+
+        try (FileInputStream fis = new FileInputStream(file);
+             ZipInputStream zis = new ZipInputStream(fis)) {
+            ZipEntry entry = zis.getNextEntry();
+            while (entry != null) {
+                result.put(entry.getName(), new String(zis.readAllBytes()));
+
+                entry = zis.getNextEntry();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return result;
+    }
+
+    public List<ZipEntry> getZipEntries() {
+        List<ZipEntry> result = new ArrayList<>();
+        
+        try (FileInputStream fis = new FileInputStream(file);
+             ZipInputStream zis = new ZipInputStream(fis)) {
+            ZipEntry entry = zis.getNextEntry();
+            while (entry != null) {
+                result.add(entry);
+                entry = zis.getNextEntry();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return result;
+    }
+
+
+
+}
