3f519b991c14a540927ccd3c20df6fad5510dacb
martin <martingeorgiev2307@gmail.com>
2026-05-11 19:32:40 +0300

    feat: god forbit I doubt Gradle again: Full Gradle pipeline for compiling Rust and making Java FFI interloop

--- src/main/java/net/sleepy/Test4.java
+++ src/main/java/net/sleepy/Test4.java
@@ -1,5 +1,20 @@
-import net.sleepy.main.java.PriceStat;
+package net.sleepy;
 
-void main () {
-    System.out.println(PriceStat.add(20,220));
+import net.sleepy.nativebind.bindings_h;
+import java.nio.file.Paths;
+
+public class Test4 {
+    static {
+        // No path or extension needed; Java looks for 'lib' + 'price_stat' + '.so'
+        System.loadLibrary("price_stat");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Result: " + bindings_h.add_numbers(5, 10));
+
+        try (var arena = java.lang.foreign.Arena.ofConfined()) {
+            var cString = arena.allocateFrom("Hello from the JVM!");
+            bindings_h.print_message(cString);
+        }
+    }
 }
--- src/main/rust/src/lib.rs
+++ src/main/rust/src/lib.rs
@@ -1,4 +1,14 @@
+use std::ffi::CStr;
+use std::os::raw::c_char;
+
 #[unsafe(no_mangle)]
 pub extern "C" fn add_numbers(a: i32, b: i32) -> i32 {
     a + b
+}
+#[unsafe(no_mangle)]
+pub extern "C" fn print_message(c_buf: *const c_char) {
+    let c_str = unsafe { CStr::from_ptr(c_buf) };
+    if let Ok(s) = c_str.to_str() {
+        println!("Rust received: {}", s);
+    }
 }
