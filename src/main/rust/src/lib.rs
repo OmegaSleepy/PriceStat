99ac44e3145406299d397d233ade41d4f092e19b
martin <martingeorgiev2307@gmail.com>
2026-05-13 17:54:06 +0300

    feat: Rust method for reading a file called from Java

--- src/main/rust/src/lib.rs
+++ src/main/rust/src/lib.rs
@@ -12,3 +12,20 @@ pub extern "C" fn print_message(c_buf: *const c_char) {
         println!("Rust received: {}", s);
     }
 }
+
+#[unsafe(no_mangle)]
+pub extern "C" fn read_and_print(c_buf: *const c_char) {
+    let file_name_c = unsafe { CStr::from_ptr(c_buf) };
+
+    if let Ok(file_name) = file_name_c.to_str() {
+        let result = fs::read(Path::new(file_name));
+
+        match result {
+            Ok(file_contents) => {
+                let msg = String::from_utf8(file_contents).unwrap();
+                println!("{}", msg);
+            }
+            Err(e) => eprintln!("Error when reading file {} \n {} ", file_name, e)
+        }
+    }
+}
