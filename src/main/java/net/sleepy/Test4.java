80898fc2ac67e59638c5fd7305873b7bbf4da3fe
martin <martingeorgiev2307@gmail.com>
2026-05-17 17:27:15 +0300

    feat: better runner, but it crashes my computer

--- src/main/java/net/sleepy/Test4.java
+++ src/main/java/net/sleepy/Test4.java
@@ -1,20 +1,41 @@
 package net.sleepy;
 
-import net.sleepy.nativebind.bindings_h;
-import java.nio.file.Paths;
+import net.sleepy.jobs.DateZipJob;
+import net.sleepy.util.DateUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 
 public class Test4 {
+    private static final Logger log = LoggerFactory.getLogger(Test4.class);
+
     static {
-        // No path or extension needed; Java looks for 'lib' + 'price_stat' + '.so'
         System.loadLibrary("price_stat");
     }
 
-    public static void main(String[] args) {
-        System.out.println("Result: " + bindings_h.add_numbers(5, 10));
 
-        try (var arena = java.lang.foreign.Arena.ofConfined()) {
-            var cString = arena.allocateFrom("Hello from the JVM!");
-            bindings_h.print_message(cString);
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        var list = DateUtils.getDatesInRange(
+                LocalDate.of(2025, 11, 1),
+                LocalDate.now()
+        );
+
+        Semaphore connectionSemaphore = new Semaphore(5);
+
+        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            list.forEach(date -> executor.submit(() -> {
+                try {
+                    // Pass the semaphore to the job
+                    new DateZipJob(date, connectionSemaphore).run();
+                } catch (InterruptedException e) {
+                    log.error(e.getMessage(), e);
+                }
+            }));
         }
     }
 }
