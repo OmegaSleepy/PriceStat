5adcaf25308dd0444defe8c6c6c117806628163c
martin <martingeorgiev2307@gmail.com>
2026-05-17 17:26:09 +0300

    feat: updated the DateZipJob, but it crashes my computer hehehe

--- src/main/java/net/sleepy/jobs/DateZipJob.java
+++ src/main/java/net/sleepy/jobs/DateZipJob.java
@@ -8,32 +8,48 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.Duration;
+import java.time.Instant;
+
+import java.util.concurrent.Semaphore;
 
 public class DateZipJob {
-
     private final String date;
+    private final Semaphore semaphore; // Add this
     private final String BASEURL = "https://kolkostruva.bg/opendata_files/%s.zip";
     private final Logger log = LoggerFactory.getLogger(DateZipJob.class);
 
-    public DateZipJob (String date) {
+    // Update constructor
+    public DateZipJob (String date, Semaphore semaphore) {
         this.date = date;
+        this.semaphore = semaphore;
     }
 
-    public void run () throws IOException, InterruptedException {
+    public void run () throws InterruptedException {
         Path path = Path.of("download/zip/" + date + ".zip");
 
         if (!Files.exists(path)) {
-            var rawZip = NetworkingClient.get(String.format(BASEURL, date));
-            Files.createDirectories(path.getParent());
-            Files.write(path, rawZip);
+            byte[] rawZip = null;
+
+            // Acquire a permit before hitting the network
+            semaphore.acquire();
+            try {
+                rawZip = NetworkingClient.get(String.format(BASEURL, date)).orElseThrow();
+                Files.createDirectories(path.getParent());
+                Files.write(path, rawZip);
+            } catch (IOException e) {
+                log.error(e.getMessage());
+                return;
+            } finally {
+                // ALWAYS release the permit in a finally block
+                semaphore.release();
+            }
         }
 
+        var start = Instant.now();
         RustFFM.parseZip(path);
-
-    }
-
-    public String getDate () {
-        return date;
+        var end = Instant.now();
+        log.info("Took {}ms in rust", (Duration.between(start, end).toMillis()));
     }
 }
 
